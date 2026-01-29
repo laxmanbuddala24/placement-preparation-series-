57cbdbee625e0029ecbf93a0bb1ae77fb5be9d61
laxmanbuddala24 <laxmanbuddala2404@gmail.com>
2026-01-29 14:44:26 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+# include <iostream>
+using namespace std;
+int main () {
+    int store_whole_number;
+    store_whole_number = 143;
+    float store_decimal_number;
+    store_decimal_number = 2.0;
+    char store_one_character;
+    store_one_character = 'I';
+    string store_multiple_character;
+    store_multiple_character = " Sir its ";
+    bool right_or_wrong;
+    right_or_wrong = true;
+    cout<<store_one_character;
+    cout<<store_whole_number;
+    cout<<store_decimal_number;
+    cout<<store_multiple_character;
+    cout<<right_or_wrong;
+    return 0;
+}
