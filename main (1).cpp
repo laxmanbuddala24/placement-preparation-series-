c7189529c927a877c078e0c632c2f9309aef197b
laxmanbuddala24 <laxmanbuddala2404@gmail.com>
2026-01-29 14:52:29 +0530

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,22 @@
+/* program for creating own  meaning full variables of basic all data types and storing values in it
+and using cout  displaying the stored values */
+# include <iostream>
+using namespace std;
+int main () {
+    int store_whole_number;
+    store_whole_number = 143; // read 143 as "I Love You" and after this there is a gap 
+    float store_decimal_number;
+    store_decimal_number = 2.0;
+    char store_one_character;
+    store_one_character = 'I';
+    string store_multiple_character;
+    store_multiple_character = " Sir its ";
+    bool right_or_wrong;
+    right_or_wrong = true;
+    cout<<store_one_character;
+    cout<<store_whole_number;
+    cout<<store_decimal_number;
+    cout<<store_multiple_character;
+    cout<<right_or_wrong;
+    return 0;
+}
